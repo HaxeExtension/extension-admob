03567e31695565ea166fe0ca604807cf45046dd7
Federico Bricker <fbricker@gmail.com>
2015-01-06 00:18:23 -0300

    Fix build for arm64, x86_64.

--- buildPackage.sh
+++ buildPackage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 dir=`dirname "$0"`
 cd "$dir"
+rm -rf project/obj
+lime rebuild . ios
+rm -rf project/obj
 rm -f openfl-admob.zip
 zip -0r openfl-admob.zip extension haxelib.json include.xml project ndll dependencies 
