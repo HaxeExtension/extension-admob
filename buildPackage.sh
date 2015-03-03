eda60a8ea35f202e90609dc87c9627d6c6c9cb0e
Federico Bricker <fbricker@gmail.com>
2015-03-03 19:27:59 -0300

    compress the haxelib

--- buildPackage.sh
+++ buildPackage.sh
@@ -5,4 +5,4 @@ rm -rf project/obj
 lime rebuild . ios
 rm -rf project/obj
 rm -f openfl-admob.zip
-zip -0r openfl-admob.zip extension haxelib.json include.xml project ndll dependencies frameworks
+zip -r openfl-admob.zip extension haxelib.json include.xml project ndll dependencies frameworks
