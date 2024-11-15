c2e2a759be754b6e9ccb8f9cc36b96ab305cd626
Lily <egzozu.be.bas@gmail.com>
2024-11-15 20:04:40 +0300

    grahhhh

--- source/macros/IOSMacro.hx
+++ source/macros/IOSMacro.hx
@@ -5,8 +5,8 @@ class IOSMacro
 {
     public static function setIOSEnvTable():Void
     {
-        Sys.command("export LD_RUNPATH_SEARCH_PATHS=$(inherited) /usr/lib/swift");
-        Sys.command("export LIBRARY_SEARCH_PATHS=$(TOOLCHAIN_DIR)/usr/lib/swift/$(PLATFORM_NAME) $(TOOLCHAIN_DIR)/usr/lib/swift-5.0/$(PLATFORM_NAME) $(inherited)");
+        Sys.command("export", ["LD_RUNPATH_SEARCH_PATHS=\"$(inherited)", "/usr/lib/swift\""]);
+        Sys.command("export", ["LIBRARY_SEARCH_PATHS=\"$(TOOLCHAIN_DIR)/usr/lib/swift/$(PLATFORM_NAME)", "$(TOOLCHAIN_DIR)/usr/lib/swift-5.0/$(PLATFORM_NAME)", "$(inherited)\""]);
     }
 }
 #end
