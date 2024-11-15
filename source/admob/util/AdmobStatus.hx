64280e121f08dd5f1016231a95d2ef305c4e7cb6
Lily <egzozu.be.bas@gmail.com>
2024-11-15 20:54:57 +0300

    format more codes

--- source/admob/util/AdmobStatus.hx
+++ source/admob/util/AdmobStatus.hx
@@ -6,11 +6,9 @@ import openfl.events.EventDispatcher;
  * Admob status
  * @author Pozirk Games (http://www.pozirk.com)
  */
-class AdmobStatus extends EventDispatcher
-{
-	public function onStatus(status:String, data:String = null):Void
-	{
-		//trace("onStatus: ", status, data);
+class AdmobStatus extends EventDispatcher {
+	public function onStatus(status:String, data:String = null):Void {
+		// trace("onStatus: ", status, data);
 		var ae:AdmobEvent = new AdmobEvent(status, data);
 		this.dispatchEvent(ae);
 	}
