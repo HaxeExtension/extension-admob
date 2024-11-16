f42acdbfce9873a536046b07ae4104d36d2357a9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-16 14:59:41 +0200

    Create AdmobConsent.hx

---
+++ admob/AdmobConsent.hx
@@ -0,0 +1,8 @@
+package admob;
+
+enum abstract AdmobConsent(String) from String to String
+{
+	final FULL = "1111111111";
+	final PERSONALIZED = "1111001011";
+	final NON_PERSONALIZED = "1100001011";
+}
