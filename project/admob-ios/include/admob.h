5c36a6af471552bb89d441151c6832c988b040fa
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-20 15:36:04 +0000

    Format

--- project/admob-ios/include/admob.h
+++ project/admob-ios/include/admob.h
@@ -2,19 +2,19 @@
 #include <vector>
 
 namespace admob
-{	
-	void init(bool testingAds, bool childDirected, bool enableRDP);
-	void initMobileAds(bool testingAds, bool childDirected, bool enableRDP, bool requestIDFA);
-	void showBanner(const char *id, int size, int align);
-	void hideBanner();
-	void loadInterstitial(const char *id);
-	void showInterstitial();
-	void loadRewarded(const char* id);
-	void showRewarded();
-	void setVolume(float vol);
-	void setVolume(float vol);
-	int hasConsentForPuprpose(int purpose);
-	const char* getConsent();
-	int isPrivacyOptionsRequired();
-	void showPrivacyOptionsForm();
-}
+{
+void init(bool testingAds, bool childDirected, bool enableRDP);
+void initMobileAds(bool testingAds, bool childDirected, bool enableRDP, bool requestIDFA);
+void showBanner(const char *id, int size, int align);
+void hideBanner();
+void loadInterstitial(const char *id);
+void showInterstitial();
+void loadRewarded(const char *id);
+void showRewarded();
+void setVolume(float vol);
+void setVolume(float vol);
+int hasConsentForPuprpose(int purpose);
+const char *getConsent();
+int isPrivacyOptionsRequired();
+void showPrivacyOptionsForm();
+} // namespace admob
