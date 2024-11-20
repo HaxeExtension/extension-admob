c7a4d925dd963fdd7b842f73708b345b34132db2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-20 21:27:04 +0200

    Update admob.h

--- project/admob-ios/include/admob.h
+++ project/admob-ios/include/admob.h
@@ -1,20 +1,17 @@
+#pragma once
+
 #include <string>
 #include <vector>
 
-namespace admob
-{
-void init(bool testingAds, bool childDirected, bool enableRDP);
-void initMobileAds(bool testingAds, bool childDirected, bool enableRDP, bool requestIDFA);
-void showBanner(const char *id, int size, int align);
-void hideBanner();
-void loadInterstitial(const char *id);
-void showInterstitial();
-void loadRewarded(const char *id);
-void showRewarded();
-void setVolume(float vol);
-void setVolume(float vol);
-int hasConsentForPuprpose(int purpose);
-const char *getConsent();
-int isPrivacyOptionsRequired();
-void showPrivacyOptionsForm();
-} // namespace admob
+void initAdmob(bool testingAds, bool childDirected, bool enableRDP);
+void showAdmobBanner(const char *id, int size, int align);
+void hideAdmobBanner();
+void loadAdmobInterstitial(const char *id);
+void showAdmobInterstitial();
+void loadAdmobRewarded(const char *id);
+void showAdmobRewarded();
+void setAdmobVolume(float vol);
+int hasAdmobConsentForPuprpose(int purpose);
+const char *getAdmobConsent();
+int isAdmobPrivacyOptionsRequired();
+void showAdmobPrivacyOptionsForm();
