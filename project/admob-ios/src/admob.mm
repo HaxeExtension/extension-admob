11d0d2f6590754413cec9eb8946605ef23e8cee8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-20 21:47:21 +0200

    Update admob.mm

--- project/admob-ios/src/admob.mm
+++ project/admob-ios/src/admob.mm
@@ -8,112 +8,112 @@ static GADRewardedAd *rewardedAd = nil;
 
 void initAdmob(bool testingAds, bool childDirected, bool enableRDP)
 {
-	GADMobileAds.sharedInstance.requestConfiguration.tagForChildDirectedTreatment(childDirected);
+    GADMobileAds.sharedInstance.requestConfiguration.tagForChildDirectedTreatment = @(childDirected);
 
-	if (testingAds)
-		GADMobileAds.sharedInstance.requestConfiguration.testDeviceIdentifiers = @[ kGADSimulatorID ];
+    if (testingAds)
+        GADMobileAds.sharedInstance.requestConfiguration.testDeviceIdentifiers = @[ GADSimulatorID ];
 
-	[GADMobileAds.sharedInstance startWithCompletionHandler:nil];
+    [GADMobileAds.sharedInstance startWithCompletionHandler:nil];
 }
 
 void showAdmobBanner(const char *id, int size, int align)
 {
-	if (bannerView != nil)
-		return;
+    if (bannerView != nil)
+        return;
 
-	dispatch_async(dispatch_get_main_queue(), ^{
-		UIViewController *rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
-		bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
-		bannerView.adUnitID = [NSString stringWithUTF8String:id];
-		bannerView.rootViewController = rootVC;
-		[rootVC.view addSubview:bannerView];
-		GADRequest *request = [GADRequest request];
-		[bannerView loadRequest:request];
-	});
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
+        bannerView = [[GADBannerView alloc] initWithAdSize:GADAdSizeBanner];
+        bannerView.adUnitID = [NSString stringWithUTF8String:id];
+        bannerView.rootViewController = rootVC;
+        [rootVC.view addSubview:bannerView];
+        GADRequest *request = [GADRequest request];
+        [bannerView loadRequest:request];
+    });
 }
 
 void hideAdmobBanner()
 {
-	dispatch_async(dispatch_get_main_queue(), ^{
-		if (bannerView != nil)
-		{
-			[bannerView removeFromSuperview];
-			bannerView = nil;
-		}
-	});
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (bannerView != nil)
+        {
+            [bannerView removeFromSuperview];
+            bannerView = nil;
+        }
+    });
 }
 
 void loadAdmobInterstitial(const char *id)
 {
-	NSString *adUnitID = [NSString stringWithUTF8String:id];
-	GADRequest *request = [GADRequest request];
+    NSString *adUnitID = [NSString stringWithUTF8String:id];
+    GADRequest *request = [GADRequest request];
 
-	[GADInterstitialAd loadWithAdUnitID:adUnitID request:request completionHandler:^(GADInterstitialAd *ad, NSError *error)
-	{
-		if (error) {
-			interstitialAd = nil;
-		} else {
-			interstitialAd = ad;
-		}
-	}];
+    [GADInterstitialAd loadWithAdUnitID:adUnitID request:request completionHandler:^(GADInterstitialAd *ad, NSError *error)
+    {
+        if (error) {
+            interstitialAd = nil;
+        } else {
+            interstitialAd = ad;
+        }
+    }];
 }
 
 void showAdmobInterstitial()
 {
-	dispatch_async(dispatch_get_main_queue(), ^{
-		if (interstitialAd != nil) {
-			UIViewController *rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
-			[interstitialAd presentFromRootViewController:rootVC];
-		}
-	});
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (interstitialAd != nil) {
+            UIViewController *rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
+            [interstitialAd presentFromRootViewController:rootVC];
+        }
+    });
 }
 
 void loadAdmobRewarded(const char *id)
 {
-	NSString *adUnitID = [NSString stringWithUTF8String:id];
-	GADRequest *request = [GADRequest request];
-	[GADRewardedAd loadWithAdUnitID:adUnitID
-							 request:request
-				   completionHandler:^(GADRewardedAd *ad, NSError *error) {
-		if (error) {
-			rewardedAd = nil;
-		} else {
-			rewardedAd = ad;
-		}
-	}];
+    NSString *adUnitID = [NSString stringWithUTF8String:id];
+    GADRequest *request = [GADRequest request];
+    [GADRewardedAd loadWithAdUnitID:adUnitID
+                             request:request
+                   completionHandler:^(GADRewardedAd *ad, NSError *error) {
+        if (error) {
+            rewardedAd = nil;
+        } else {
+            rewardedAd = ad;
+        }
+    }];
 }
 
 void showAdmobRewarded()
 {
-	dispatch_async(dispatch_get_main_queue(), ^{
-		if (rewardedAd != nil) {
-			UIViewController *rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
-			[rewardedAd presentFromRootViewController:rootVC
-							  userDidEarnRewardHandler:^{
-				GADAdReward *reward = rewardedAd.adReward;
-			}];
-		}
-	});
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (rewardedAd != nil) {
+            UIViewController *rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
+            [rewardedAd presentFromRootViewController:rootVC
+                              userDidEarnRewardHandler:^{
+                GADAdReward *reward = rewardedAd.adReward;
+            }];
+        }
+    });
 }
 
 void setAdmobVolume(float vol)
 {
-	GADMobileAds.sharedInstance.applicationVolume = vol;
+    GADMobileAds.sharedInstance.applicationVolume = vol;
 }
 
 int hasAdmobConsentForPuprpose(int purpose)
 {
-	return 1;
+    return 1;
 }
 
 const char *getAdmobConsent()
 {
-	return "CONSENTED";
+    return "CONSENTED";
 }
 
 bool isAdmobPrivacyOptionsRequired()
 {
-	return false;
+    return false;
 }
 
 void showAdmobPrivacyOptionsForm()
