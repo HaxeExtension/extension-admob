f9fe90b2909bea732b677e9c1de2469b1d14d5ed
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-24 06:09:16 +0000

    Format a little

--- admob/AdmobConsent.hx
+++ admob/AdmobConsent.hx
@@ -2,7 +2,7 @@ package admob;
 
 enum abstract AdmobConsent(String) from String to String
 {
-	final FULL = "1111111111";
-	final PERSONALIZED = "1111001011";
-	final NON_PERSONALIZED = "1100001011";
+	final FULL = '1111111111';
+	final PERSONALIZED = '1111001011';
+	final NON_PERSONALIZED = '1100001011';
 }
