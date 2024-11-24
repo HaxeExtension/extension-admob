d6cd4ded82ece4c1447888836b3dc1404d361e60
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-24 21:40:55 +0200

    Update Admob.java

--- project/admob-android/src/main/java/org/haxe/extension/Admob.java
+++ project/admob-android/src/main/java/org/haxe/extension/Admob.java
@@ -43,32 +43,6 @@ import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-	You can use the Android Extension class in order to hook
-	into the Android activity lifecycle. This is not required
-	for standard Java code, this is designed for when you need
-	deeper integration.
-
-	You can access additional references from the Extension class,
-	depending on your needs:
-
-	- Extension.assetManager (android.content.res.AssetManager)
-	- Extension.callbackHandler (android.os.Handler)
-	- Extension.mainActivity (android.app.Activity)
-	- Extension.mainContext (android.content.Context)
-	- Extension.mainView (android.view.View)
-
-	You can also make references to static or instance methods
-	and properties on Java classes. These classes can be included
-	as single files using <java path="to/File.java" /> within your
-	project, or use the full Android Library Project format (such
-	as this example) in order to include your own AndroidManifest
-	data, additional dependencies, etc.
-
-	These are also optional, though this example shows a static
-	function for performing a single task, like returning a value
-	back to Haxe from Java.
-*/
 public class Admob extends Extension
 {
 	public static AdView adView;
