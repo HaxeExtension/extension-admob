6bb30869f7e8acfa89ad150e17acb6cdb9ccea1a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-25 07:08:58 +0200

    Create ATTStatus.hx

---
+++ admob/ATTStatus.hx
@@ -0,0 +1,10 @@
+package admob;
+
+enum abstract ATTStatus(String) from String to String
+{
+	final NOT_DETERMINED = 'NOT_DETERMINED';
+	final RESTRICTED = 'RESTRICTED';
+	final DENIED = 'DENIED';
+	final AUTHORIZED = 'AUTHORIZED';
+	final UNKNOWN = 'UNKNOWN';
+}
