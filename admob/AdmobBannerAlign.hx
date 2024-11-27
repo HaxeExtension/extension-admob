09092f27755cc559d1e1d9a5d2e1cffdf5ec7648
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-27 21:17:44 +0200

    Update AdmobBannerAlign.hx

--- admob/AdmobBannerAlign.hx
+++ admob/AdmobBannerAlign.hx
@@ -1,18 +1,12 @@
 package admob;
 
-#if android
-/**
- * @see https://developer.android.com/reference/android/view/Gravity
- */
 enum abstract AdmobBannerAlign(Int) from Int to Int
 {
+	#if android
 	final BOTTOM = 0x00000050 | 0x00000001; // BOTTOM | CENTER_HORIZONTAL
 	final TOP = 0x00000030 | 0x00000001; // TOP | CENTER_HORIZONTAL
-}
-#elseif ios
-enum abstract AdmobBannerAlign(Int) from Int to Int
-{
+	#elseif ios
 	final BOTTOM = 0;
 	final TOP = 1;
+	#end
 }
-#end
