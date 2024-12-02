e080b38b43d5bd8b7c76df04110eff99dee584fd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-02 15:36:05 +0200

    Update Admob.java

--- project/admob-android/src/main/java/org/haxe/extension/Admob.java
+++ project/admob-android/src/main/java/org/haxe/extension/Admob.java
@@ -579,4 +579,22 @@ public class Admob extends Extension
 			mainActivity.addContentView(adContainer, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
 		}
 	}
+
+	@Override
+	public void onPause()
+	{
+		if (adView != null)
+			adView.pause();
+
+		super.onPause();
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+
+		if (adView != null)
+			adView.resume();
+	}
 }
