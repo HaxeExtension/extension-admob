61a4540e7ee6a1470e297bb7db9631007af566ba
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-05 23:02:07 +0200

    Update JNICache.hx

--- admob/android/util/JNICache.hx
+++ admob/android/util/JNICache.hx
@@ -31,7 +31,7 @@ class JNICache
 	 * @param cache Whether to cache the result (default true).
 	 * @return A dynamic reference to the static method.
 	 */
-	public static function createStaticMethod(className:String, methodName:String, signature:String, cache:Bool = true):Dynamic
+	public static function createStaticMethod(className:String, methodName:String, signature:String, cache:Bool = true):Null<Dynamic>
 	{
 		final key:String = '$className::$methodName::$signature';
 
@@ -52,7 +52,7 @@ class JNICache
 	 * @param cache Whether to cache the result (default true).
 	 * @return A dynamic reference to the member method.
 	 */
-	public static function createMemberMethod(className:String, methodName:String, signature:String, cache:Bool = true):Dynamic
+	public static function createMemberMethod(className:String, methodName:String, signature:String, cache:Bool = true):Null<Dynamic>
 	{
 		final key:String = '$className::$methodName::$signature';
 
@@ -73,7 +73,7 @@ class JNICache
 	 * @param cache Whether to cache the result (default true).
 	 * @return A reference to the static field.
 	 */
-	public static function createStaticField(className:String, fieldName:String, signature:String, cache:Bool = true):JNIStaticField
+	public static function createStaticField(className:String, fieldName:String, signature:String, cache:Bool = true):Null<JNIStaticField>
 	{
 		final key:String = '$className::$fieldName::$signature';
 
@@ -94,7 +94,7 @@ class JNICache
 	 * @param cache Whether to cache the result (default true).
 	 * @return A reference to the member field.
 	 */
-	public static function createMemberField(className:String, fieldName:String, signature:String, cache:Bool = true):JNIMemberField
+	public static function createMemberField(className:String, fieldName:String, signature:String, cache:Bool = true):Null<JNIMemberField>
 	{
 		final key:String = '$className::$fieldName::$signature';
 
