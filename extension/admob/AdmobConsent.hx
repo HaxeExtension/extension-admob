5a673b52551d432634661b3ed717215e635d5f02
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-26 23:07:38 +0200

    Update `AdmobConsent` enum to 11-digit

--- extension/admob/AdmobConsent.hx
+++ extension/admob/AdmobConsent.hx
@@ -2,7 +2,7 @@ package extension.admob;
 
 enum abstract AdmobConsent(String) from String to String
 {
-	final FULL = '1111111111';
-	final PERSONALIZED = '1111001011';
-	final NON_PERSONALIZED = '1100001011';
+	final FULL = '11111111111';
+	final PERSONALIZED = '11110010111';
+	final NON_PERSONALIZED = '11000010111';
 }
