615158d8ab8b32c921950d393b32e418cb299604
Igor <1659590+pozirk@users.noreply.github.com>
2025-01-04 18:58:27 -0500

    Bring more comments back

--- extension/admob/AdmobConsent.hx
+++ extension/admob/AdmobConsent.hx
@@ -1,8 +1,10 @@
 package extension.admob;
 
+//https://support.google.com/admob/answer/9760862#consent-policies
+//https://iabeurope.eu/iab-europe-transparency-consent-framework-policies/#A_Purposes
 enum abstract AdmobConsent(String) from String to String
 {
-	final FULL = "11111111111";
-	final PERSONALIZED = "11110010111";
-	final NON_PERSONALIZED = "11000010111";
+	final FULL = "11111111111"; //full consent has been granted, admob should have no problems showing ads
+	final PERSONALIZED = "11110010111"; //enough consent has been granted for personalized ads, most likely will never happen, because user has to set all the checkboxes manually, and also for ads to work user has to consent to all the vendors
+	final NON_PERSONALIZED = "11000010111"; //consent to show non-personalized ads was given, there are little chances this can happen, because user has to set all the right checkboxes manually, and also for ads to work user has to consent to all the vendors
 }
