3d9012b45bd604542361c6b7a60d668a3533fe04
Igor <1659590+pozirk@users.noreply.github.com>
2025-01-04 19:59:44 -0500

    Bring back adaptive; remove skyscraper, not really supported

--- extension/admob/AdmobBannerSize.hx
+++ extension/admob/AdmobBannerSize.hx
@@ -2,13 +2,12 @@ package extension.admob;
 
 enum abstract AdmobBannerSize(Int) from Int to Int
 {
-	final BANNER = 0; //320x50
+	final ADAPTIVE = 0; //Anchored adaptive, (a replacement for SMART_BANNER), banner width is fullscreen, height calculated acordingly (might not work well in landscape orientation)
+	final BANNER = 1; //320x50
 	final FULL_BANNER = 2; //468x60
 	final LARGE_BANNER = 3; //320x100
 	final LEADERBOARD = 4; //728x90
 	final MEDIUM_RECTANGLE = 5; //300x250
-#if android
-	final FLUID = 1; //Android only. A dynamically sized banner that matches its parent's width and expands/contracts its height to match the ad's content after loading completes.
-	final WIDE_SKYSCRAPER = 6; //160x600, Android only.
-#end
+	final FLUID = 6; //A dynamically sized banner that matches its parent's width and expands/contracts its height to match the ad's content after loading completes.
+	//final WIDE_SKYSCRAPER = 7; //mediation only, not currently supportred by Google Mobile Ads network: https://developers.google.com/android/reference/com/google/android/gms/ads/AdSize#public-static-final-adsize-wide_skyscraper
 }
